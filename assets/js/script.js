5b0b45ab0d4881fab8b01e3fdc4ab09a8f8ac2b2
DJ <douglass.dj@gmail.com>
2018-07-17 14:47:29 -0500

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var ajax = new XMLHttpRequest();
+ajax.open("GET", "assets/img/meeting-room.svg", true);
+ajax.send();
+ajax.onload = function(e) {
+  var div = document.createElement("div");
+  div.innerHTML = ajax.responseText;
+  document.body.insertBefore(div, document.body.childNodes[0]);
+}
